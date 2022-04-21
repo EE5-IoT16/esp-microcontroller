126a43d81df0e04a482baef8b0597d86a430effa
Yifeng Chen <yif916032@gmail.com>
2022-04-21 12:32:59 +0200

    update

--- main/main.c
+++ main/main.c
@@ -41,21 +41,25 @@ void tPulse(void* arg)
     vTaskDelete(NULL);
 
 }
-
+void tBlufi(void* arg)
+{
+    run_blufi();
+    vTaskDelete(NULL);
+}
 
 void app_main(void)
 {
     
     ESP_LOGI(TAG, "main process starts to run");
-    /*
+    
     controller.sensorC = 1;
     controller.counterC = 1;
+    xTaskCreatePinnedToCore(tBlufi,"blufi",4096,NULL,2,NULL,tskNO_AFFINITY);
     xTaskCreatePinnedToCore(tSensor,"sensor",2048,NULL,1,NULL,tskNO_AFFINITY);
-    xTaskCreatePinnedToCore(tPulse,"pulse",2048,NULL,2,NULL,tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(tPulse,"pulse",2048,NULL,1,NULL,tskNO_AFFINITY);
     vTaskDelay(300000/ portTICK_PERIOD_MS);
     controller.sensorC = 0;
     controller.counterC = 0;
-    */
+    
     //sample_api_req_hardcoded(50, 12345);
-    run_blufi();
 }
